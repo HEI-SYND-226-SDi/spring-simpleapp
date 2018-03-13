e7b7f51913e2bf3ae85d0bf5911dd07844838c43
Michael Clausen <michael.clausen@hevs.ch>
2018-03-13 20:24:37 +0100

    Removed unnecessary annotations (default values)

--- src/main/java/ch/hevs/sdi/simpleapp/SimpleappConfiguration.java
+++ src/main/java/ch/hevs/sdi/simpleapp/SimpleappConfiguration.java
@@ -2,14 +2,10 @@ package ch.hevs.sdi.simpleapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class SimpleappConfiguration {
     @Bean
-    @Scope("singleton")
-    @Lazy(false)
     Printer printer() {
         return new FancyPrinter();
     }
