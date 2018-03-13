b2334299d93f19016ed1d7f4ef2725b4cd26a0a8
Michael Clausen <michael.clausen@hevs.ch>
2018-03-13 20:25:48 +0100

    The Printer instance created depends the actual profile

--- src/main/java/ch/hevs/sdi/simpleapp/SimpleappConfiguration.java
+++ src/main/java/ch/hevs/sdi/simpleapp/SimpleappConfiguration.java
@@ -1,12 +1,18 @@
 package ch.hevs.sdi.simpleapp;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class SimpleappConfiguration {
     @Bean
+    @Profile("default")
     Printer printer() {
+        return new SimplePrinter();
+    }
+
+    @Bean
+    @Profile("fancy")
+    Printer fancyPrinter() {
         return new FancyPrinter();
     }
 }
